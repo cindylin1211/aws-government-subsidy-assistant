30653e73adfded95ca48a069444d3e30aa419184
cindylin1211 <lin3042123@gmail.com>
2025-11-11 00:01:07 +0800

    更新 Footer 聯絡信箱為 cindyjw@amazon.com

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer className="bg-gray-100 py-4 mt-12">
       <div className="container mx-auto px-4 text-center text-sm text-gray-600">
         <div className="space-y-2">
-          <p>聯絡信箱：aws-tw-dgr@amazon.com</p>
+          <p>聯絡信箱：cindyjw@amazon.com</p>
           <p>&copy; 2025 AWS 政府補助小助手 - 內部工具</p>
         </div>
       </div>
